26503e814cf7003342c39719d03de9b988555a23
Veera Sivarajan <sveera.2001@gmail.com>
2020-09-01 12:07:29 +0530

    Added: Five Round Loop

--- game.hs
+++ game.hs
@@ -28,11 +28,17 @@ gameLogic Scissor choice | choice == Rock = -1
 options :: [Choice]
 options = [Rock, Paper, Scissor]
        
-game :: Choice -> Choice -> IO ()
-game c1 c2 = do putStrLn "Game Begins"
-            let result = gameLogic c1 c2 
-                    winner  | result > 0 = putStrLn "Player1 wins"
-                            | result < 0 = putStrLn "Player2 wins"
-                            | otherwise  = putStrLn "Draw"
-                winner
+game :: Choice -> Choice -> Int -> IO ()
+game c1 c2 count = do putStrLn $ "Round: " ++ show count
+                      let result = gameLogic c1 c2 
+                          winner  | result > 0 = putStrLn "Player1 wins"
+                                  | result < 0 = putStrLn "Player2 wins"
+                                  | otherwise  = putStrLn "Draw"
+                      if count < 5
+                      then do winner
+                              count <- return (count + 1)
+                              game c1 c2 count
+                              else putStrLn "game over"
 
+start :: Choice -> Choice -> IO ()
+start input1 input2 = game input1 input2 0
